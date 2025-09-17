350ff2c301acb80c8dac93fc2de326d4050bc395
zachshattuck19-glitch <zachshattuck19@gmail.com>
2025-09-16 18:53:27 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('fetch', e=>{e.respondWith(fetch(e.request))});
+const CACHE='bbuddy-cache-v1-3-4';
+const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.json'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))) });
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))) });
